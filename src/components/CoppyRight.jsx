2fa9697c28f94008016696922296b662433edf41
Raktim Shuvrow <shuvrow98@gmail.com>
2025-07-13 03:07:31 +0600

    add some text size

--- src/components/CoppyRight.jsx
+++ src/components/CoppyRight.jsx
@@ -1,9 +1,9 @@
 
 function CoppyRight() {
   return (
-    <div className="text-center text-white py-4 text-[20px] leading-[32px]">
+    <div className="text-center text-white py-4 xl:text-[20px] text-[14px] leading-[32px]">
       <p>
-        ©️ Copyright | All Rights Reserved | <span className="text-blue-400 text-2xl">Raktim Shuvrow </span>juni@r Devloper
+        ©️ Copyright | All Rights Reserved | <span className="text-blue-400 xl:text-2xl text-[14px]">Raktim Shuvrow </span>juni@r Devloper
       </p>
     </div>
   )
